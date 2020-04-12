3032aef4942d01acf4e07cda2239fd566f8db203
Boas Pucker <36674497+bpucker@users.noreply.github.com>
2020-04-12 18:15:55 +0200

    Add files via upload

---
+++ get_cons_pos.py
@@ -0,0 +1,101 @@
+### Boas Pucker ###
+### bpucker@cebitec.uni-bielefeld.de ###
+### v0.1 ###
+
+__usage__ = """
+					python get_cons_pos.py
+					--in <INPUT_FASTA_FILE>
+					--out <OUTPUT_FOLDER>
+					--ref <REF_SEQ_NAME>
+					
+					optional:
+					--mafft <PATH_TO_MAFFT>[mafft]
+					--mincons <FLOAT, CONSERVATION_OF_RESIDUE_TO_BE_CONSIDERED>[1.0]
+					
+					bug reports and feature requests: bpucker@cebitec.uni-bielefeld.de
+					"""
+
+import os, sys
+
+# --- end of imports --- #
+
+def load_sequences( fasta_file ):
+	"""! @brief load candidate gene IDs from file """
+	
+	sequences = {}
+	
+	with open( fasta_file ) as f:
+		header = f.readline()[1:].strip()
+		seq = []
+		line = f.readline()
+		while line:
+			if line[0] == '>':
+					sequences.update( { header: "".join( seq ) } )
+					header = line.strip()[1:]
+					seq = []
+			else:
+				seq.append( line.strip() )
+			line = f.readline()
+		sequences.update( { header: "".join( seq ) } )	
+	return sequences
+
+
+def get_conserved_pos( alignment, mincons, ref_name ):
+	"""! @brief identify conserved positions in reference sequence """
+	
+	cons_positions = []
+	for idx, aa in enumerate( alignment[ ref_name ] ):
+		counter = 0
+		for seq in alignment.values():
+			if seq[ idx ] == aa:
+				counter += 1
+		if counter / len( alignment.values() ) >= mincons:
+			cons_positions.append( aa + str( idx+1 ) )
+	return cons_positions
+
+
+def main( arguments ):
+	"""! @brief run everything """
+	
+	input_file = arguments[ arguments.index('--in')+1 ]
+	output_folder = arguments[ arguments.index('--out')+1 ]
+	ref_name = arguments[ arguments.index('--ref')+1 ]
+	
+	if '--mafft' in arguments:
+		mafft = arguments[ arguments.index('--out')+1 ]
+	else:
+		mafft = "mafft"
+	if '--mincons' in arguments:
+		mincons = float( arguments[ arguments.index('--mincons')+1 ] )
+	else:
+		mincons = 1.0
+	
+	if not os.path.exists( output_folder ):
+		os.makedirs( output_folder )
+	
+	# --- check for presence of reference sequence in file --- #
+	peps = load_sequences( input_file )
+	try:
+		peps[ ref_name ]
+	except KeyError:
+		sys.exit( "ERROR: Reference sequence name " + ref_name + " not detected." )
+	
+	# --- generate alignment --- #
+	aln_file = output_folder + "alignment.fasta.aln"
+	if not os.path.isfile( aln_file ):
+		os.popen( mafft + " " + input_file + " > " + aln_file )
+	
+	# --- load alignment and identify conserved residues --- #
+	alignment = load_sequences( aln_file )
+	cons_positions = get_conserved_pos( alignment, mincons, ref_name )
+	print "number of identified conserved amino acid residues: " + str( len( cons_positions ) )
+	
+	output_file = output_folder + "conserved_residues.txt"
+	with open( output_file, "w" ) as out:
+		out.write( "\t".join( [ ref_name ] + cons_positions ) + '\n' )
+
+
+if '--in' in sys.argv and '--out' in sys.argv and '--ref' in sys.argv:
+	main( sys.argv )
+else:
+	sys.exit( __usage__ )
